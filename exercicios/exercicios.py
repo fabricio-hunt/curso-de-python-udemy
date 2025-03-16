75372b77f32e044324634da5e4d0b9737cf3ee7a
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-03-16 00:17:22 -0300

    exercicios

---
+++ exercicios/exercicios.py
@@ -0,0 +1,38 @@
+import math
+
+'Faça um programa que leia um número inteiro e imprima-o.'
+
+numero = int(input('Digite um numero inteiro: '))
+print(f'O numero inteiro digitado foi: {numero}')
+
+
+"Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles."
+numero1 = int(input('Digite um numero inteiro: '))
+numero2 = int(input('Digite um numero inteiro: '))
+numero3 = int(input('Digite um numero inteiro: '))
+
+soma = numero1 + numero2 + numero3
+
+print(f'A soma entre os números é de: {soma}')
+
+
+import math  # Importação da biblioteca (opcional, pois não usaremos math.sqrt())
+
+numero1 = int(input('Digite um número inteiro: '))
+numero2 = int(input('Digite um número inteiro: '))
+numero3 = int(input('Digite um número inteiro: '))
+
+# Calculando os quadrados corretamente
+quadrado1 = numero1 ** 2
+quadrado2 = numero2 ** 2
+quadrado3 = numero3 ** 2
+
+# Soma dos quadrados
+soma = quadrado1 + quadrado2 + quadrado3
+
+print(f'A soma dos quadrados dos números é: {soma}')
+
+
+
+
+
