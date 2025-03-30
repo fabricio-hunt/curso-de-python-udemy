44abe1188a1b6a56a68b9558b52c7314cf2c97ea
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-03-29 21:15:37 -0300

    Estruturas Condicionais

--- exercicios/exercicios.py
+++ exercicios/exercicios.py
@@ -5,7 +5,6 @@ import math
 numero = int(input('Digite um numero inteiro: '))
 print(f'O numero inteiro digitado foi: {numero}')
 
-
 "Faça um programa que peça para o usuário digitar três valores inteiro e imprima a soma deles."
 numero1 = int(input('Digite um numero inteiro: '))
 numero2 = int(input('Digite um numero inteiro: '))
@@ -16,18 +15,14 @@ soma = numero1 + numero2 + numero3
 print(f'A soma entre os números é de: {soma}')
 
 
-import math  # Importação da biblioteca (opcional, pois não usaremos math.sqrt())
-
 numero1 = int(input('Digite um número inteiro: '))
 numero2 = int(input('Digite um número inteiro: '))
 numero3 = int(input('Digite um número inteiro: '))
 
-# Calculando os quadrados corretamente
 quadrado1 = numero1 ** 2
 quadrado2 = numero2 ** 2
 quadrado3 = numero3 ** 2
 
-# Soma dos quadrados
 soma = quadrado1 + quadrado2 + quadrado3
 
 print(f'A soma dos quadrados dos números é: {soma}')
